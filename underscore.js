bdad3913bc06145ff32758383908175793bd4d86
Sheykoslav <mazurec777@gmail.com>
2019-06-07 16:11:52 +0300

    first commit

---
+++ underscore.js
@@ -0,0 +1,15 @@
+function Car(brand, model, year){
+  this.brandName = brand;
+  this.model = model;
+  this.productionYear = year;
+}
+var car = _.create(Car.prototype, {brandName:'Lamborghini',model:'Veneno',productionYear:2013});
+var invertedCar = _.invert(car);
+
+console.log('Using pick:');
+var pickedFromCar = _.pick(car,'brandName','productionYear');
+console.log(pickedFromCar);
+
+console.log('Using omit:');
+var omitedInCar = _.omit(car, 'model');
+console.log(omitedInCar);
